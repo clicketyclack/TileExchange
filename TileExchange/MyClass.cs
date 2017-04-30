adfa1390bc3ffaf84221f9d21dc19200fe253cb9
Erik Mossberg <erik.mossberg@gmail.com>
2017-04-30 13:17:50 +0200

    Create file tree structure for unit tests. Refactor: Move unit test code to new structure.

--- TileExchange/MyClass.cs
+++ TileExchange/MyClass.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using NUnit.Framework;
+
 using System.IO;
 using System.Drawing;
 using System.Reflection;
@@ -32,39 +32,5 @@ namespace TileExchange
 		}
 	}
 
-	/// <summary>
-	/// This fixture only verifies that project imports and dependencies work.
-	/// Created due to some issues with dependencies not matching my .net target profile.
-	/// </summary>
-	[TestFixture]
-	public class PackageReferenceTests
-	{
 
-		/// <summary>
-		/// Test that ImageProcessor works via a rgba -> hsl conversion.
-		/// </summary>
-		[Test]
-		public void UseRgbaObjects()
-		{
-
-			var rgba = ImageProcessor.Imaging.Colors.RgbaColor.FromRgba(222, 205, 171, 255);
-			var hsl = ImageProcessor.Imaging.Colors.HslaColor.FromColor(rgba);
-
-			Assert.IsTrue(39.9 / 360.0 <= hsl.H && hsl.H <= 40.1 / 360.0);
-
-
-
-		}
-		/// <summary>
-		/// Run POC method. Will fail if png read/write starts causing issues.
-		/// 
-		/// If your tests fail here, check that "assets" and "output" paths exist.
-		/// </summary>
-		[Test]
-		public void AlterImagePOC()
-		{
-			var poc = new MyClass();
-			poc.AlterImagePOC();
-		}
-	}
 }
--- TileExchange/UnitTests/Images/ImageEditingTests.cs
+++ TileExchange/UnitTests/Images/ImageEditingTests.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using NUnit.Framework;
+
 namespace TileExchange
 {
 	public class ImageEditingTests
 	{
 		public ImageEditingTests()
 		{
+		}
+
+		/// <summary>
+		/// Run POC method. Will fail if png read/write starts causing issues.
+		/// 
+		/// If your tests fail here, check that "assets" and "output" paths exist.
+		/// </summary>
+		[Test]
+		public void AlterImagePOC()
+		{
+			var poc = new MyClass();
+			poc.AlterImagePOC();
 		}
 	}
 }
