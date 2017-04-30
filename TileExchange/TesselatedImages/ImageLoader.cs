d26a14f855ab94162497015ff00ab3cbf86e766a
Erik Mossberg <erik.mossberg@gmail.com>
2017-04-30 18:46:43 +0200

    Add support for loading fragmented images.

---
+++ TileExchange/TesselatedImages/ImageLoader.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace TileExchange.TesselatedImages
+{
+
+	/// <summary>
+	/// Tesselated image. Combines a standard image (png etc) with a set of fragments.
+	/// The fragments represent tiles or puzzle pieces.
+	/// </summary>
+	public interface ITesselatedImage
+	{
+		List<IFragment> GetFragments();
+	}
+
+	class TesselatedImage : ITesselatedImage
+	{
+
+		private Bitmap bitmap;
+		private List<IFragment> fragments;
+		public TesselatedImage(Bitmap bitmap, List<IFragment> fragments)
+		{
+			this.bitmap = bitmap;
+			this.fragments = fragments;
+		}
+
+		public List<IFragment> GetFragments()
+		{
+			return fragments;
+		}
+	}
+
+	/// <summary>
+	/// Representation of a single puzzle piece.
+	/// </summary>
+	public interface IFragment
+	{
+		
+		Size GetSize();
+	}
+
+	public class Square16Fragment : IFragment
+	{
+		public Size GetSize()
+		{
+			return new Size { Width = 16, Height = 16 };
+		}
+	}
+
+	public class CustomRectangleFragment : IFragment
+	{
+
+		private Size size;
+		public CustomRectangleFragment(Size size)
+		{
+			this.size = size;
+		}
+
+		public Size GetSize()
+		{
+			return size;
+		}
+	}
+
+	/// <summary>
+	/// Load an image.
+	/// </summary>
+	public class TesselatedImageLoader
+	{
+		private string project_path;
+		private string images_path;
+		public TesselatedImageLoader()
+		{
+			project_path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			images_path = String.Format("{0}/../../../assets/images/", project_path);
+		}
+
+		private Bitmap LoadBitmap(string filename) 
+		{
+			
+			var to_open = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(images_path), filename));
+			var loaded = new Bitmap(to_open);
+			return loaded;
+		}
+
+		public ITesselatedImage LoadFromImagelibrary(string filename, ITesselator tesselation_tactic)
+		{
+			var bitmap = LoadBitmap(filename);
+			var toreturn = new TesselatedImage(bitmap, tesselation_tactic.FragmentImage(bitmap));
+			return toreturn;
+		}
+
+	}
+
+	/// <summary>
+	/// Write out the image to disk.
+	/// </summary>
+	public class ImageWriter
+	{
+		private string project_path;
+		private string output_path;
+		public ImageWriter()
+		{
+			project_path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			output_path = String.Format("{0}/../../../output/", project_path);
+		}
+
+		public void WriteBitmap(Bitmap bitmap, string filename)
+		{
+			var destination = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(output_path), "altered.png"));
+			bitmap.Save(destination);
+		}
+
+	}
+
+	public interface ITesselator
+	{
+		List<IFragment> FragmentImage(Bitmap bitmap);
+	}
+
+	/// <summary>
+	/// Bitmap tesselator.
+	/// </summary>
+	public class SingleFragmentTesselator : ITesselator
+	{
+		public List<IFragment> FragmentImage(Bitmap bitmap)
+		{
+			
+			var toreturn = new List<IFragment>();
+			toreturn.Add(new CustomRectangleFragment(bitmap.Size));
+			return toreturn;
+		}
+	}
+}
+
---
+++ TileExchange/UnitTests/LoadAndTesselate/LoadingBasics.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using TileExchange.TesselatedImages;
+
+namespace TileExchange
+{
+	/// <summary>
+	/// Verifies basics of image loading. We should be able to find and open images from the assets directory.
+	/// </summary>
+	[TestFixture]
+	public class LoadingBasics
+	{
+		public LoadingBasics()
+		{
+		}
+		/// <summary>
+		/// Verify that we can load images with single fragments.
+		/// </summary>
+		[Test]
+		[TestCase("blue_building.jpg", 650, 1057 )]
+		[TestCase("green_leaf.jpg", 1119, 697)]
+		[TestCase("red_blue_transitions.jpg", 448, 448)]
+		public void SingleFragmentImages(string imagename, int width, int height)
+		{
+			
+			var til = new TesselatedImageLoader();
+			var sft = new SingleFragmentTesselator();
+
+			var loaded_image = til.LoadFromImagelibrary(imagename, sft);
+			var fragments = loaded_image.GetFragments();
+
+			Assert.AreEqual(1, fragments.Count);
+
+			var fragment = fragments[0];
+			Assert.AreEqual(width, fragment.GetSize().Width);
+			Assert.AreEqual(height, fragment.GetSize().Height);
+
+		}
+			
+
+	}
+
+
+
+}
